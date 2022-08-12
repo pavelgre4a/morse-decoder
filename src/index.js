2de739fa656ada6b0fa141cbb148cad902b1ec52
pavelgre4a <pavelgre4a@gmail.com>
2022-08-12 16:07:34 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const letterList = new Array
+
+    for (let i = 0; i < expr.length; i += 10) {
+        letterList.push(expr.slice(i, i + 10))
+    }
+
+    letterList.forEach((encodedLetter, index) => {
+        if (encodedLetter === '**********') {
+            letterList[index] = ' '
+            return
+        }
+
+        characterList = new Array
+
+        for (let i = 0; i < encodedLetter.length; i += 2) {
+            characterList.push(encodedLetter.slice(i, i + 2))
+        }
+
+        characterList = characterList.filter(item => item !== '00')
+
+        characterList.forEach((char, index) => {
+            if (char === '10') {
+                characterList[index] = '.'
+            } else {
+                characterList[index] = '-'
+            }
+        })
+
+        letterList[index] = MORSE_TABLE[characterList.join('')]
+    })
+
+    return letterList.join('')
 }
 
 module.exports = {
